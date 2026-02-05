817c9cfb07db60e797d5e29e7beac85bc4c13bf8
oartuner <oartuner@gmail.com>
2026-02-05 22:38:07 +0300

    refactor: Remove Turkish, set English as default language (EN/DE only)

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import tr from './locales/tr.json';
 import en from './locales/en.json';
 import de from './locales/de.json';
 
 const resources = {
-    tr: { translation: tr },
     en: { translation: en },
     de: { translation: de }
 };
@@ -15,11 +13,12 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: localStorage.getItem('language') || 'tr',
-        fallbackLng: 'tr',
+        lng: localStorage.getItem('language') || 'en',
+        fallbackLng: 'en',
         interpolation: {
             escapeValue: false
         }
     });
 
 export default i18n;
+
